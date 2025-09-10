4398256e8fc0242c92c204848244482fe7cc5329
Anoop Rehman <anoop.reh@gmail.com>
2025-09-10 12:54:04 +0000

    ci: add volume mount for faster development iteration

--- benchmark/run_benchmark.sh
+++ benchmark/run_benchmark.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 docker run --gpus=all \
-    -v ${HOME}/.cache/huggingface:/root/.cache/huggingface \
-    --shm-size 64g \
-    cgen:latest \
-    python3 benchmark/benchmark_cgen.py \
-    benchmark/configs/llama2-7b-2xl4.json \
-    --dataset sharegpt \
-    --dataset-path /datasets/sharegpt.json \
-    --num-requests 50 \
-    --hf-token ${HF_TOKEN} \
-    --print-output
+   -v .:/workspace/cgen \
+   -v ${HOME}/.cache/huggingface:/root/.cache/huggingface \
+   --shm-size 64g \
+   cgen:latest \
+   python3 benchmark/benchmark_cgen.py \
+   benchmark/configs/llama2-7b-2xl4.json \
+   --dataset sharegpt \
+   --dataset-path /datasets/sharegpt.json \
+   --num-requests 50 \
+   --hf-token ${HF_TOKEN} \
+   --print-output
